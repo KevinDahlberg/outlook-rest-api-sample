fd1f9c19d457090307b48dcc5522f9d986110388
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-10-31 12:18:27 -0500

    router file created

---
+++ server.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var url = require('url');
+
+function start(route, handle) {
+  function onRequest(req, res) {
+    var pathName = url.parse(request.url).pathname;
+    console.log('Request for ' + pathName + ' received.');
+    route(handle, pathName, res, req);
+  }
+
+  var port = 8000;
+  http.createServer(onRequest).listen(port);
+  console.log('Server on!  Listening on port: ', port, '...');
+}
+
+exports.start = start;
